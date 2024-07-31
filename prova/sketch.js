ad5882f9b2a0d8d53f0254df0f61de86128576f1
Mathieu Linty <mathieu.linty@studio.unibo.it>
2024-07-31 12:27:38 +0200

    add: 'prova' project

--- prova/sketch.js
+++ prova/sketch.js
@@ -1,24 +1,12 @@
 // Title: "prova"
-// Created: sab 27 lug 2024, 12:03:24, CEST
+// Created: mer 31 lug 2024, 12:27:34, CEST
 
-const is_uppercase = (ch) => {
-  let n = ch.charCodeAt(0);
-  return (n >= 65 && n <= 90);
-}
-const is_lowercase = (ch) => {
-  let n = ch.charCodeAt(0);
-  return (n >= 97 && n <= 122);
-}
-const is_number = (ch) => {
-  let n = ch.charCodeAt(0);
-  return (n >= 48 && n <= 57);
-}
-const is_valid = (ch) => {
-  return (is_lowercase(ch) || is_uppercase(ch) || is_number(ch) || ch === '_')
+function setup()
+{
+  createCanvas(600, 600);
 }
 
-const username = "pippo";
-
-for (let ch of username) {
-  is_valid(ch) ? console.log(ch + " is valid") : console.log(ch + " is not valid");
+function draw()
+{
+  background(50, 180, 0);
 }
